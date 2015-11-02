18c0bdbf68e5b7a2ebf9d7411c89eddfddc432d2
KeithCa <c12406518@mydit.ie>
2015-11-02 16:47:42 +0000

    Update container-server.py

--- container-server.py
+++ container-server.py
@@ -78,7 +78,7 @@ def containers_log(id):
 
     """
     output = docker('logs')
-    resp = json.dumps(docker_logs_to_object(output, '0506d303e1e6'))
+    resp = json.dumps(docker_logs_to_object('0506d303e1e6', output))
     return Response(response=resp, mimetype="application/json")
 
 
